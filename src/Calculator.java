46e35572fa880cc33130614eae6028e948f4f59d
Jo Eun Hyeong <whdmsgud8@naver.com>
2024-07-26 11:57:59 +0900

    Level2 1번 요구사항

---
+++ src/Calculator.java
@@ -0,0 +1,36 @@
+class DivisionByZeroException {
+    public DivisionByZeroException(String message) {
+        System.out.println(message);
+    }
+
+}
+
+public class Calculator {
+
+    public void Calculator(int firstNum, int secondNum, String operator) {
+        if (secondNum == 0) {
+            int result;
+
+            switch (operator) {
+                case "+":
+                    result = firstNum + secondNum;
+                    break;
+                case "-":
+                    result = firstNum - secondNum;
+                    break;
+                case "*":
+                    result = firstNum * secondNum
+                    break;
+                case "/":
+                    if (secondNum == 0) {
+                        new DivisionByZeroException("0으로 나눌 수 없습니다.");
+                    }
+                    result = firstNum / secondNum;
+                    break;
+            }
+        }
+
+    }
+
+}
+
